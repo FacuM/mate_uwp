33f3fc8d6afcf20e0068c0b4be1f67fe18fe1435
FacuM <facumo.fm@gmail.com>
2019-04-22 03:00:22 -0300

    Implement the server as a background task.
    This patch fixes a bug where the app wouldn't show a toast after its lifecycle changes from "Running" to "Suspended".
    
    Other minor changes are detailed below:
    
    - Small visual improvements.

--- Mate/MainPage.xaml.cs
+++ Mate/MainPage.xaml.cs
@@ -1,29 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Notifications;
-using Microsoft.Toolkit.Uwp.Notifications;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using Windows.Networking.Sockets;
 using Windows.Networking;
-using Windows.Storage.Streams;
-using Windows.UI.ViewManagement;
-using Windows.ApplicationModel.Activation;
 using Windows.Networking.Connectivity;
+using Windows.ApplicationModel.Background;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications;
+using Windows.Networking.Sockets;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 namespace Mate
@@ -34,20 +19,8 @@ namespace Mate
 
     public sealed partial class MainPage : Page
     {
-        private StreamSocketListener mStreamSocketListener;
-        private TextBlock mServerStatusTextBlock;
-        private const int SERVER_PORT = 26566;
-
-        public async void startUp()
-        {
-            var hostName = new HostName("localhost");
-            var socket = new StreamSocket();
-
-            mStreamSocketListener = new StreamSocketListener();
-
-            mStreamSocketListener.ConnectionReceived += StreamSocketListener_ConnectionReceived;
-            await mStreamSocketListener.BindServiceNameAsync(SERVER_PORT.ToString()).AsTask();
-        }
+        private string mSocketId = "MateServer";
+        private int mTransferOwnershipCount = 0;
 
         private void StreamSocketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
         {
@@ -82,33 +55,96 @@ namespace Mate
             ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
 
+        private async void TransferOwnership(StreamSocketListener streamSocketListener)
+        {
+            await streamSocketListener.CancelIOAsync();
+
+            var dataWriter = new DataWriter();
+            ++mTransferOwnershipCount;
+            dataWriter.WriteInt32(mTransferOwnershipCount);
+            var context = new SocketActivityContext(dataWriter.DetachBuffer());
+            streamSocketListener.TransferOwnership(mSocketId, context);
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            TransferOwnership(mStreamSocketListener);
+            deferral.Complete();
+        }
+
+        private async void startServerAsync()
+        {
+            Debug.WriteLine("Starting server...");
+
+            mStreamSocketListener = new StreamSocketListener();
+            mStreamSocketListener.EnableTransferOwnership(mTask.TaskId, SocketActivityConnectedStandbyAction.DoNotWake);
+
+            mStreamSocketListener.ConnectionReceived += StreamSocketListener_ConnectionReceived;
+            await mStreamSocketListener.BindServiceNameAsync(SERVER_PORT.ToString()).AsTask();
+
+            if (mStreamSocketListener == null)
+            {
+                mServerStatusTextBlock.Text = "Hubo un problema al iniciar el servidor.";
+            }
+            else
+            {
+                mServerStatusTextBlock.Text = "El servidor está ejecutándose en " + SERVER_IP + ".";
+            }
+
+            Debug.WriteLine("Startup completed.");
+        }
+
+        private StreamSocketListener       mStreamSocketListener;
+        private TextBlock                  mServerStatusTextBlock;
+        private BackgroundTaskRegistration mTask;
+
+        private string                     SERVER_IP               = null;
+        private const int                  SERVER_PORT             = 26566;
+
         public MainPage()
         {
             this.InitializeComponent();
 
+            var socket = new StreamSocket();
+            var socketTaskBuilder = new BackgroundTaskBuilder()
+            {
+                Name = "MateNotificationListener",
+                TaskEntryPoint = "Tasks.BackgroundTaskServerRunner"
+            };
+
+            var trigger = new SocketActivityTrigger();
+            socketTaskBuilder.SetTrigger(trigger);
+
+            // Some background tasks could be kept running after shutdown (cause Windows), lets unregister them.
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                task.Value.Unregister(true);
+            }
+
+            // Then, register it.
+            mTask = socketTaskBuilder.Register();
+
             mServerStatusTextBlock = (TextBlock)FindName("ServerStatusTextBlock");
 
-
             var internetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            foreach (HostName hostName in NetworkInformation.GetHostNames())
+            {
+                if (hostName.Type == HostNameType.Ipv4)
+                {
+                    SERVER_IP = hostName.ToString();
+                    break;
+                }
+            }
+
             if (internetConnectionProfile.NetworkAdapter == null)
             {
                 mServerStatusTextBlock.Text = "Para poder ejecutar esta aplicación, tenés que conectarte a una red.";
             }
             else
             {
-                startUp();
-
-                String SERVER_IP = null;
-                foreach (HostName hostName in NetworkInformation.GetHostNames())
-                {
-                    if (hostName.Type == HostNameType.Ipv4)
-                    {
-                        SERVER_IP = hostName.ToString();
-                        break;
-                    }
-                }
-
-                mServerStatusTextBlock.Text = "El servidor está ejecutándose en " + SERVER_IP + ":" + SERVER_PORT + ".";
+                startServerAsync();
             }
         }
     }
